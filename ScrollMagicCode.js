0cecec896fa2897e9e43ccc7a5be00123ec5b085
waleed negm <68160452+waleed-negm@users.noreply.github.com>
2022-11-11 15:18:45 +0200

    add preload and change hero text svg

--- ScrollMagicCode.js
+++ ScrollMagicCode.js
@@ -1,70 +1,84 @@
 // @ts-nocheck
 
+document.onreadystatechange = () => {
+	if (document.readyState === 'complete') {
+		console.log('document ready');
+		document.body.style.overflow = 'auto';
+		window.scrollTo({
+			top: 0,
+		});
+		document.querySelector('.preloadingPage').style.opacity = '0';
+		document.querySelector('.preloadingPage').style.zIndex = '99';
+	} else document.body.style.overflow = 'hidden';
+};
+
 var controller = new ScrollMagic.Controller();
 
 addEventListener('click', () => console.log('test'));
 //hero image
 //pin
-new ScrollMagic.Scene({triggerHook: 0, duration: 2500, tweenChanges: true, triggerElement: '.parallax'}).addIndicators().setPin('.parallax', {pushFollowers: true}).addTo(controller);
+new ScrollMagic.Scene({ triggerHook: 0, duration: 2500, tweenChanges: true, triggerElement: '.parallax' }).addIndicators().setPin('.parallax', { pushFollowers: true }).addTo(controller);
 //zoom out
-const imageTween = new TimelineMax().add(TweenMax.fromTo('.myimage', 1, {transform: 'scale(1.5)'}, {transform: 'scale(1)', ease: Linear.Circ})).add(TweenMax.to('.myimage', 1, {ease: Linear.Circ}), 0);
-new ScrollMagic.Scene({triggerHook: 0, duration: 2500, tweenChanges: true, triggerElement: '.parallax'}).addIndicators().setTween(imageTween).addTo(controller);
+const imageTween = new TimelineMax()
+	.add(TweenMax.fromTo('.myimage', 1, { transform: 'scale(1.5)' }, { transform: 'scale(1)', ease: Linear.Circ }))
+	.add(TweenMax.to('.myimage', 1, { ease: Linear.Circ }), 0);
+new ScrollMagic.Scene({ triggerHook: 0, duration: 2500, tweenChanges: true, triggerElement: '.parallax' }).addIndicators().setTween(imageTween).addTo(controller);
 //draw path
 const myPath = document.querySelector('#word');
 var length = myPath.getTotalLength();
 myPath.style.strokeDasharray = length;
 myPath.style.strokeDashoffset = length;
-const tween = new TimelineMax().add(TweenMax.to(myPath, 1, {strokeDasharray: 0, strokeDashoffset: 0, ease: Linear.Circ})).add(TweenMax.to(myPath, 1, {ease: Linear.Circ}), 0);
-new ScrollMagic.Scene({triggerHook: 0, duration: 2000, tweenChanges: true, triggerElement: '.parallax'}).addIndicators().setTween(tween).addTo(controller);
+const tween = new TimelineMax().add(TweenMax.to(myPath, 1, { strokeDasharray: 0, strokeDashoffset: 0, ease: Linear.Circ })).add(TweenMax.to(myPath, 1, { ease: Linear.Circ }), 0);
+new ScrollMagic.Scene({ triggerHook: 0, duration: 2000, tweenChanges: true, triggerElement: '.parallax' }).addIndicators().setTween(tween).addTo(controller);
 //blur
-const tween3 = new TimelineMax().add(TweenMax.to('.myimage', 1, {filter: 'blur(2px)', ease: Linear.Circ})).add(TweenMax.to('.myimage', 1, {ease: Linear.Circ}), 0);
-new ScrollMagic.Scene({triggerHook: 0, duration: 2000, tweenChanges: true, triggerElement: '.parallax'}).addIndicators().setTween(tween3).addTo(controller);
+const tween3 = new TimelineMax().add(TweenMax.to('.myimage', 1, { filter: 'blur(2px)', ease: Linear.Circ })).add(TweenMax.to('.myimage', 1, { ease: Linear.Circ }), 0);
+new ScrollMagic.Scene({ triggerHook: 0, duration: 2000, tweenChanges: true, triggerElement: '.parallax' }).addIndicators().setTween(tween3).addTo(controller);
 //welcome
 //pin
-new ScrollMagic.Scene({triggerHook: 0, duration: '100%', tweenChanges: true, triggerElement: '.welcome'}).addIndicators().setPin('.welcome', {pushFollowers: true}).addTo(controller);
+new ScrollMagic.Scene({ triggerHook: 0, duration: '100%', tweenChanges: true, triggerElement: '.welcome' }).addIndicators().setPin('.welcome', { pushFollowers: true }).addTo(controller);
 //draw path
 const welcomePath = document.querySelector('#welcomeline');
 var welcomePathlength = welcomePath.getTotalLength();
 welcomePath.style.strokeDasharray = welcomePathlength;
 welcomePath.style.strokeDashoffset = welcomePathlength;
-const welcomePathtween = new TimelineMax().add(TweenMax.to(welcomePath, 1, {strokeDashoffset: 0, ease: Linear.Circ})).add(TweenMax.to(welcomePath, 1, {ease: Linear.Circ}), 0);
-new ScrollMagic.Scene({triggerHook: 0, duration: '100%', tweenChanges: true, triggerElement: '.welcome'}).addIndicators().setTween(welcomePathtween).addTo(controller);
+const welcomePathtween = new TimelineMax().add(TweenMax.to(welcomePath, 1, { strokeDashoffset: 0, ease: Linear.Circ })).add(TweenMax.to(welcomePath, 1, { ease: Linear.Circ }), 0);
+new ScrollMagic.Scene({ triggerHook: 0, duration: '100%', tweenChanges: true, triggerElement: '.welcome' }).addIndicators().setTween(welcomePathtween).addTo(controller);
 //pin hwm
-new ScrollMagic.Scene({triggerHook: 0, duration: '100%', tweenChanges: true, triggerElement: '.hwm'}).addIndicators().setPin('.hwm', {pushFollowers: true}).addTo(controller);
+new ScrollMagic.Scene({ triggerHook: 0, duration: '100%', tweenChanges: true, triggerElement: '.hwm' }).addIndicators().setPin('.hwm', { pushFollowers: true }).addTo(controller);
 //pin timer
-new ScrollMagic.Scene({triggerHook: 0, duration: '100%', tweenChanges: true, triggerElement: '.timer'}).addIndicators().setPin('.timer', {pushFollowers: true}).addTo(controller);
+new ScrollMagic.Scene({ triggerHook: 0, duration: '100%', tweenChanges: true, triggerElement: '.timer' }).addIndicators().setPin('.timer', { pushFollowers: true }).addTo(controller);
 //pin location
-new ScrollMagic.Scene({triggerHook: 0, duration: '100%', tweenChanges: true, triggerElement: '.location'}).addIndicators().setPin('.location', {pushFollowers: true}).addTo(controller);
+new ScrollMagic.Scene({ triggerHook: 0, duration: '100%', tweenChanges: true, triggerElement: '.location' }).addIndicators().setPin('.location', { pushFollowers: true }).addTo(controller);
 //pin footer
-new ScrollMagic.Scene({triggerHook: 0, duration: 1000, tweenChanges: true, triggerElement: '.footer'}).addIndicators().setPin('.footer', {pushFollowers: true}).addTo(controller);
+new ScrollMagic.Scene({ triggerHook: 0, duration: 1000, tweenChanges: true, triggerElement: '.footer' }).addIndicators().setPin('.footer', { pushFollowers: true }).addTo(controller);
 //draw thank
 const thankPath = document.querySelector('#thank');
 var thanklength = thankPath.getTotalLength();
 thankPath.style.strokeDasharray = thanklength;
 thankPath.style.strokeDashoffset = thanklength;
-const thanktween = new TimelineMax().add(TweenMax.to(thankPath, 1, {strokeDasharray: 0, strokeDashoffset: 0, ease: Linear.Circ})).add(TweenMax.to(thankPath, 1, {ease: Linear.Circ}), 0);
-new ScrollMagic.Scene({triggerHook: 0, duration: 1000, tweenChanges: true, triggerElement: '.footer'}).addIndicators().setTween(thanktween).addTo(controller);
+const thanktween = new TimelineMax().add(TweenMax.to(thankPath, 1, { strokeDasharray: 0, strokeDashoffset: 0, ease: Linear.Circ })).add(TweenMax.to(thankPath, 1, { ease: Linear.Circ }), 0);
+new ScrollMagic.Scene({ triggerHook: 0, duration: 1000, tweenChanges: true, triggerElement: '.footer' }).addIndicators().setTween(thanktween).addTo(controller);
 //-----------------------------------------------------------------------------------------------
 const countdown = () => {
-    const countDate = new Date('December 1,2022 19:00:00').getTime();
-    const now = new Date().getTime();
-    const gap = countDate - now;
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-    if (gap > 0) {
-        document.querySelector('.daysCount').innerText = Math.floor(gap / day);
-        document.querySelector('.hoursCount').innerText = Math.floor((gap % day) / hour);
-        document.querySelector('.minutesCount').innerText = Math.floor((gap % hour) / minute);
-        document.querySelector('.secondsCount').innerText = Math.floor((gap % minute) / second);
-    } else {
-        clearInterval(counter);
-        document.querySelector('.daysCount').innerText =
-            document.querySelector('.hoursCount').innerText =
-            document.querySelector('.minutesCount').innerText =
-            document.querySelector('.secondsCount').innerText =
-                0;
-    }
+	const countDate = new Date('December 1,2022 19:00:00').getTime();
+	const now = new Date().getTime();
+	const gap = countDate - now;
+	const second = 1000;
+	const minute = second * 60;
+	const hour = minute * 60;
+	const day = hour * 24;
+	if (gap > 0) {
+		document.querySelector('.daysCount').innerText = Math.floor(gap / day);
+		document.querySelector('.hoursCount').innerText = Math.floor((gap % day) / hour);
+		document.querySelector('.minutesCount').innerText = Math.floor((gap % hour) / minute);
+		document.querySelector('.secondsCount').innerText = Math.floor((gap % minute) / second);
+	} else {
+		clearInterval(counter);
+		document.querySelector('.daysCount').innerText =
+			document.querySelector('.hoursCount').innerText =
+			document.querySelector('.minutesCount').innerText =
+			document.querySelector('.secondsCount').innerText =
+				0;
+	}
 };
 var counter = setInterval(() => countdown(), 1000);
